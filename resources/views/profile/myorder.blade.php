4c825e252cc5242182fc536b87aaa550ea3f8b0f
kukujiabo <kukujiabo@163.com>
2015-09-24 16:39:51 +0800

    order_list

--- resources/views/profile/myorder.blade.php
+++ resources/views/profile/myorder.blade.php
@@ -1,8 +1,10 @@
-
 @extends('profile/profile')
 
 @section('subcontent')
+
 @include('modal-box')
+
+
 <div class="sub-wrapper">
   <h4>我的订单</h4>
 </div>
@@ -30,66 +32,9 @@
       <div style="clear:both"></div>
     </div>
     <div class="order-content" id="order-list">
-        @foreach ($orders as $order)
-        <div class="order-title">
-          <div class="col-xs-4">
-            <b>编号：{{$order->order_code}} </b>
-          </div>
-          <div class="col-xs-4">
-            <b>{{$order->created_at}}</b>
-          </div>
-          <div style="clear:both"></div>
-        </div>
-        <div class="order-body">
-          <div class="col-xs-3 no-padding">
-            <div class="col-xs-5 padding-5">
-              <img src="{{$order->good_tiny_pic}}" width="100%">
-            </div>
-            <div class="col-xs-7 padding-5">
-              {{$order->good_name}}
-            </div> 
-          </div>
-          <div class="col-xs-2 order-col">
-            {{$order->sum/$order->num}}
-          </div>
-          <div class="col-xs-1 order-col">
-            {{$order->num}}
-          </div>
-          <div class="col-xs-1 order-col">
-            {{$order->cut_fee}}
-          </div>
-          <div class="col-xs-2 order-col">
-            {{$order->final_price}}
-          </div>
-          <div class="col-xs-1 order-col">
-            @if ($order->status == 0)
-            <a href="{{asset('order/pay')}}?order={{$order->order_code}}" class="require go-to-pay" data-id="{{$order->oid}}">
-               未付款
-            </a>
 
-            @elseif ($order->status == 1)
-            已付款
-            @elseif ($order->status == 2)
-              已完成
-              <div>
-                <a href="#" class="theme-font-blue deliver-info" data-id="{{$order->order_code}}">
-                  <span class="glyphicon glyphicon-info"></span>
-                  查看物流
-                </a>
-              </div>
+      @include('profile/order_list')
 
-            @elseif ($order->status == 3)
-              已取消
-
-            @endif
-          </div>
-          <div class="col-xs-2 order-col">
-            上海
-          </div>
-          <div style="clear:both"></div>
-        </div> 
-      @endforeach
-      </div>
     </div>    
   </div>
   <div class="col-md-12 text-center">
---
+++ resources/views/profile/order_list.blade.php
@@ -0,0 +1,59 @@
+@foreach ($orders as $order)
+<div class="order-title">
+  <div class="col-xs-4">
+    <b>编号：{{$order->order_code}} </b>
+  </div>
+  <div class="col-xs-4">
+    <b>{{$order->created_at}}</b>
+  </div>
+  <div style="clear:both"></div>
+</div>
+<div class="order-body">
+  <div class="col-xs-3 no-padding">
+    <div class="col-xs-5 padding-5">
+      <img src="{{$order->good_tiny_pic}}" width="100%">
+    </div>
+    <div class="col-xs-7 padding-5">
+      {{$order->good_name}}
+    </div> 
+  </div>
+  <div class="col-xs-2 order-col">
+    {{$order->sum/$order->num}}
+  </div>
+  <div class="col-xs-1 order-col">
+    {{$order->num}}
+  </div>
+  <div class="col-xs-1 order-col">
+    {{$order->cut_fee}}
+  </div>
+  <div class="col-xs-2 order-col">
+    {{$order->final_price}}
+  </div>
+  <div class="col-xs-1 order-col">
+    @if ($order->status == 0)
+    <a href="{{asset('order/pay')}}?order={{$order->order_code}}" class="require go-to-pay" data-id="{{$order->oid}}">
+       未付款
+    </a>
+
+    @elseif ($order->status == 1)
+    已付款
+    @elseif ($order->status == 2)
+      已完成
+      <div>
+        <a href="#" class="theme-font-blue deliver-info" data-id="{{$order->order_code}}">
+          <span class="glyphicon glyphicon-info"></span>
+          查看物流
+        </a>
+      </div>
+
+    @elseif ($order->status == 3)
+      已取消
+
+    @endif
+  </div>
+  <div class="col-xs-2 order-col">
+    上海
+  </div>
+  <div style="clear:both"></div>
+</div> 
+@endforeach
