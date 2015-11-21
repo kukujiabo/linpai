f2a2d9594d7978aeb417c95d8bad2d5ba695869f
kukujiabo <kukujiabo@163.com>
2015-11-21 17:01:38 +0800

    321

--- resources/views/mobile/mobile.blade.php
+++ resources/views/mobile/mobile.blade.php
@@ -10,7 +10,7 @@
   <meta name="description" content="首页描述" />     
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />     
   <meta http-equiv="Cache-Control" content="no-cache" />     
-  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=2.0"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;"/>
   <meta name="apple-mobile-web-app-capable" content="yes" />     
   <link rel="stylesheet" href="{{asset('css/jquery.mobile-1.4.5.min.css')}}" >
   <link rel="stylesheet" href="{{asset('css/mobile.css')}}">
