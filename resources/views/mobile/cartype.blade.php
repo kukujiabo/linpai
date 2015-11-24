7fddc1c9841f70cc67e7486432d31020db8f83aa
kukujiabo <kukujiabo@163.com>
2015-11-25 02:20:09 +0800

    312

--- resources/views/mobile/cartype.blade.php
+++ resources/views/mobile/cartype.blade.php
@@ -14,8 +14,8 @@
   <div data-role="content">
     <h3>请选择车辆类型：</h3>
     <div style="margin-top:10%;">
-      <a href="/miniorder/buy?car_hand=1" role="button" class="ui-btn  ui-shadow ui-corner-all " >新车</a>
-      <a href="/miniorder/buy?car_hand=2" style="margin-top:5%" role="button" class="ui-btn  ui-shadow ui-corner-all " >二手车</a>
+      <a href="/miniorder/buy?car_hand=1" class="ui-btn  ui-shadow ui-corner-all " >新车</a>
+      <a href="/miniorder/buy?car_hand=2" style="margin-top:5%" class="ui-btn  ui-shadow ui-corner-all " >二手车</a>
     </div>    
   </div>
   <!--
