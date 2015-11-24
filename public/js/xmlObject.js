9cd15012b19e4d9deb79a00c294a34aee5499cda
kukujiabo <kukujiabo@163.com>
2015-11-24 23:26:24 +0800

    312

--- public/js/xmlObject.js
+++ public/js/xmlObject.js
@@ -14,5 +14,11 @@ $(function(){
    
   xmlHttpRequest.open("GET","AjaxServlet",true);
   xmlHttpRequest.open("POST","AjaxServlet",true);
+
+  if (typeof xmlHttpRequest == 'undefined') {
+  
+    alert('XMLHttpRequest 对象没有生成！');
+  
+  }
    
 });
